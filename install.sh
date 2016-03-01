aa95f82e976e07c8985d2f23a0100c33e4067e3d
Marko Korhonen <marko.korhonen@druid.fi>
2016-03-01 10:18:15 +0200

    thanx xcode for making this harder

--- install.sh
+++ install.sh
@@ -2,7 +2,40 @@
 
 echo -e "\n--- Install druid-toolbelt ---\n\n"
 
+OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
 PHP_TAP="homebrew/php/php70"
+
+# Get Xcode CLI tools
+TOOLS=clitools.dmg
+if [ ! -f "$TOOLS" ]; then
+  if [ "$OSX_VERS" -eq 7 ]; then
+	  DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
+  elif [ "$OSX_VERS" -eq 8 ]; then
+  	  DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_mountain_lion_april_2013.dmg
+  elif [ "$OSX_VERS" -eq 9 ]; then
+	  DMGURL=http://adcdownload.apple.com/Developer_Tools/command_line_tools_os_x_mavericks_for_xcode__late_october_2013/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg
+  elif [ "$OSX_VERS" -eq 10 ]; then
+	  DMGURL=http://adcdownload.apple.com/Developer_Tools/Command_Line_Tools_OS_X_10.10_for_Xcode_6.3.2/commandlinetoolsosx10.10forxcode6.3.2.dmg
+  elif [ "$OSX_VERS" -eq 11 ]; then
+	  DMGURL=http://adcdownload.apple.com/Developer_Tools/Command_Line_Tools_OS_X_10.11_for_Xcode_7.2/Command_Line_Tools_OS_X_10.11_for_Xcode_7.2.dmg
+  else
+    echo -e "No supported OSX version found."
+    exit 1
+  fi
+fi
+
+curl "$DMGURL" -o "$TOOLS"
+
+TMPMOUNT=`/usr/bin/mktemp -d /tmp/clitools.XXXX`
+hdiutil attach "$TOOLS" -mountpoint "$TMPMOUNT"
+installer -pkg "$(find $TMPMOUNT -name '*.mpkg')" -target /
+hdiutil detach "$TMPMOUNT"
+rm -rf "$TMPMOUNT"
+rm "$TOOLS"
+exit
+
+echo "OSX = $OSX_VERS, url is $DMGURL"
+exit 1
 
 echo -n "Enter the PHP version you want to install (5, 7) [ENTER]: "
 read PHP_VERSION
