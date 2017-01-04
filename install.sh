c3248fd994bc6150de0cf5f829c8cd0cfd202e79
Marko Korhonen <marko.korhonen@gmail.com>
2017-01-04 22:52:53 +0200

    Update PHP 7 to 7.1

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ installOhMy () {
 installPHPandTools () {
     printSectionTitle "PHP and tools"
 
-    brew install homebrew/php/php70
+    brew install homebrew/php/php71
 
     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
     php composer-setup.php
